a6ad3d9b09896988f785b1d8483eefd1546967fd
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2020-05-30 17:02:10 +0530

    updated ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,24 +1,17 @@
 var React = require("react")
 
 // Hack, to reorder the helmet components as first in <head> tag
-exports.onPreRenderHTML = function onPreRenderHTML({
-  getHeadComponents,
-  replaceHeadComponents,
-}) {
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  /**
+   * @type {any[]} headComponents
+   */
   const headComponents = getHeadComponents()
+
   headComponents.sort((a, b) => {
-    if (a.type === b.type || (a.type !== "style" && b.type !== "style")) {
+    if (a.props && a.props["data-react-helmet"]) {
       return 0
     }
-
-    if (a.type === "style") {
-      return 1
-    } else if (b.type === "style") {
-      return -1
-    }
-
-    return 0
+    return 1
   })
-
   replaceHeadComponents(headComponents)
 }
