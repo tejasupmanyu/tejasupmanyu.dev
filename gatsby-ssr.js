01bce185bc2ed72b4bb869cb74ef2f1e2ee9b09e
Tejas-CureFit <tejas.upmanyu@v.curefit.com>
2020-05-30 17:34:51 +0530

    changes to ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,23 +1,6 @@
 const React = require("react")
 const { Helmet } = require("react-helmet")
 
-exports.onRenderBody = (
-  { setHeadComponents, setHtmlAttributes, setBodyAttributes },
-  pluginOptions
-) => {
-  const helmet = Helmet.renderStatic()
-  setHtmlAttributes(helmet.htmlAttributes.toComponent())
-  setBodyAttributes(helmet.bodyAttributes.toComponent())
-  setHeadComponents([
-    helmet.title.toComponent(),
-    helmet.link.toComponent(),
-    helmet.meta.toComponent(),
-    helmet.noscript.toComponent(),
-    helmet.script.toComponent(),
-    helmet.style.toComponent(),
-  ])
-}
-
 exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
   const headComponents = getHeadComponents()
 
